4882b66caf1ac3b1c663bfe181974a91acf29f59
lopitos <alfredolopezq01>
2019-08-29 17:49:54 -0400

    examen

---
+++ asignaciones/asignacion11/js/currentday.js
@@ -0,0 +1,22 @@
+﻿// fecha actual dinámicamente en el siguiente formato de fecha: lunes 6 de abril de 2020
+function mostrarHora() {
+    var h = new Date();
+    cad = h.getHours() + ":" + h.getMinutes() + ":" + h.getSeconds();
+    
+    var mes = h.getMonth();
+    var dia = h.getDay();
+    var diaMes = h.getDate();
+    var anio = h.getFullYear();
+    var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    var esteMes = meses[mes];
+    var diasSemana = ["Domingo", "Lunes", "Martes", "Miercoles", "jueves", "viernes", "Sabado"];
+    var diaHoy = diasSemana[dia];
+
+    fecha = diaHoy +" "+ diaMes +" "+ "de"+" " + esteMes+ " " + "del"+ " " + anio;
+    window.status = cad;
+    /*window.status = fecha;*/
+    document.getElementById('hora').innerHTML= cad;
+    setTimeout("mostrarHora()", 1000);
+    document.getElementById('fecha').innerHTML= fecha;
+}
+
